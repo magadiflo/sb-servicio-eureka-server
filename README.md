a50d0a9375b8d5c7362fb31aee6f4ecf7460908b
Martín <magadiflo@gmail.com>
2022-08-24 16:55:30 -0500

    Creando el servidor de registro Eureka

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Sección 03: Eureka Server: Registrando microservicios
+
+## Acceder al Dashboard de Eureka
+Ir a la ruta desde el navegador
+
+```
+http://localhost:8761/
+```
--- src/main/java/com/magadiflo/eureka/SbServicioEurekaServerApplication.java
+++ src/main/java/com/magadiflo/eureka/SbServicioEurekaServerApplication.java
@@ -2,7 +2,9 @@ package com.magadiflo.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer //Habilitamos Eureka Server
 @SpringBootApplication
 public class SbServicioEurekaServerApplication {
 
